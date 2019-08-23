7481372f60fc81fa6084c976261239d0d7599c39
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-23 14:45:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word (array)
+  reveresed = array.split(" ")
   return_array= []
   array.each do |array|
     return_array << array.reverse
   end 
-  return_array
+  return_array.join(" ")
 end
   
   
