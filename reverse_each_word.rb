b3f7bbd734019b683ac4456e6d7558ffd9da9b66
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-23 14:43:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word (array)
-  reveresed = array.split(" ").collect do |word|
+  reveresed = array.split(" ")
+  return_array= []
+  reversed.each do |array|
+    return_array << array.reverse 
   word.reverse 
   end
   reversed.join
